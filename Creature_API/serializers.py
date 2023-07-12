1f0d86a4d01fe55375ff45b233bb9a705ad41547
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-07-12 16:08:10 -0400

    Creature Serializer

--- Creature_API/serializers.py
+++ Creature_API/serializers.py
@@ -1,2 +1,14 @@
 from rest_framework import serializers
 from .models import Creature
+
+
+class CreatureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Creature
+        fields = (
+            'id',
+            'name',
+            'variation_type',
+            'description',
+            'image_url'
+        )
