75ae4efa3d20759bf80f3b8821c01e7e7ea9224f
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-07-12 18:34:22 -0400

    adding creatures to database

--- config/settings.py
+++ config/settings.py
@@ -142,12 +142,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'Creature--API.User'
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    )
-}
 
 CORS_ALLOW_ALL_ORIGINS = True
