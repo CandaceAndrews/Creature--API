c987f77638e264382a0ed76f129a508415730b32
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-07-12 19:19:39 -0400

    MegaCreature model created

--- Creature_API/admin.py
+++ Creature_API/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Creature
+from .models import Creature, MegaCreature
 
 admin.site.register(Creature)
+admin.site.register(MegaCreature)
--- Creature_API/models.py
+++ Creature_API/models.py
@@ -17,3 +17,13 @@ class Creature(models.Model):
 
     def __str__(self):
         return f"{self.name} - type: {self.variation_type}"
+
+
+class MegaCreature(models.Model):
+    creature = models.ForeignKey(
+        Creature, on_delete=models.CASCADE, related_name='mega_creature')
+    description = models.CharField(max_length=600)
+    image_url = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f"{self.creature.name} - type {self.creature.variation_type}"
