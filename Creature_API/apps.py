cd27eca96e1299a1abe2e47498072a249db7b435
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-07-12 16:03:44 -0400

    getting started

---
+++ Creature_API/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CreatureApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Creature_API'
---
+++ Creature_API/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Creature(models.Model):
+
+    VARIATION_CHOICES = (
+        ('A', 'A'),
+        ('B', 'B'),
+        ('C', 'C'),
+        ('D', 'D')
+    )
+    name = models.CharField(max_length=100)
+    variation_type = models.CharField(
+        choices=VARIATION_CHOICES, default='A', max_length=20)
+    description = models.CharField(max_length=600)
+    image_url = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f"{self.name} - type: {self.variation_type}"
