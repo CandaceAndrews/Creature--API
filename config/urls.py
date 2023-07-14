09ed48f49b32672458d2b67c44680cbe347f676e
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-07-14 18:10:23 -0400

    changing image location

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from Creature_API import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.CreatureListView, name='creature-list'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
