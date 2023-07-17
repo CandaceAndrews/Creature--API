eb81d690397950051d2e304cadc80c6613c9f328
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-07-17 17:35:51 -0400

    editing format

--- Creature_API/views.py
+++ Creature_API/views.py
@@ -8,3 +8,11 @@ from .serializers import CreatureSerializer, MegaCreatureSerializer
 
 def gallery(request):
     return render(request, 'Creature_API/gallery.html')
+
+
+def photo(request):
+    return render(request, 'Creature_API/photo.html')
+
+
+def add(request):
+    return render(request, 'Creature_API/add.html')
--- config/urls.py
+++ config/urls.py
@@ -15,14 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from Creature_API import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.CreatureListView, name='creature-list'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
