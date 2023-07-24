134de39aa9dcdd86352d80e0c155fc49e24979be
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-07-24 19:07:43 -0400

    configuring static photos

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 import Creature_API.urls
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(Creature_API.urls)),
